b07bf80dcf82080337031e862270af319bc4f5c7
schlui <161bschlui@eurogym.info>
2023-04-29 11:07:03 +0200

    Test

--- Abschlussprojekt2023/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.abschlussprojekt2023 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.abschlussprojekt2023 to javafx.fxml;
-    exports com.example.abschlussprojekt2023;
-}
