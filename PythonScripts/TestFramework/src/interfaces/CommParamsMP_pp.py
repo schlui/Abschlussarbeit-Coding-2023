2e451e3a3eec73e41f660c5fde8820822d5f1b2f
Luise Schedelberger <80944836+schlui@users.noreply.github.com>
2023-06-16 09:48:19 +0200

    ...

---
+++ PythonScripts/TestFramework/src/interfaces/CommParamsMP_pp.py
@@ -0,0 +1,18 @@
+# pyright: reportMissingImports = false, reportUndefinedVariable = false
+# # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# File Name:  	CommInterfaceParamMP_pp.py
+# Class Name:  	CommInterfaceParam_pp.py
+# Description:	
+# Company:	    dieEntwickler Elektronik GmbH
+# Author:	    Manfred PEIRLEITNER
+# Date:		    2022/05/18
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# ----------------------------------------------------------------------------------------------------
+# brief:        CommParams_pp
+# details:      
+# param[in]:    ---
+# param[out]:   ---
+# ----------------------------------------------------------------------------------------------------
+class CommParams_pp( object ):
+    pass;
