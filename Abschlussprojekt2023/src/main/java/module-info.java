0329c3ff876431b28ed8b2620be19c6ca3a5e4cc
Gunti1234 <92030735+gunterhard@users.noreply.github.com>
2023-06-17 11:38:55 +0200

    Datenbank korrektur
    Änderungen rückgängig gemacht - wegen Dummheit gerhardseitig

--- Abschlussprojekt2023/src/main/java/module-info.java
+++ Abschlussprojekt2023/src/main/java/module-info.java
@@ -1,7 +1,6 @@
 module com.example.abschlussprojekt2023 {
     requires javafx.controls;
     requires javafx.fxml;
-    requires java.sql;
 
 
     opens com.example.abschlussprojekt2023 to javafx.fxml;
